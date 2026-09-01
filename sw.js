c1e12203dd3880b521abf2a2bb9737764a03c68a
Maxiisanchezz <maxiisanchezz2020@gmail.com>
2026-09-01 03:29:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-const CACHE = 'credimach-v8';
-const FILES = ['./','./index.html','./manifest.json','./credimach_logo_transparent.png'];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).catch(()=>caches.match('./index.html'))));
-});
+const CACHE='credimach-v10';
+const FILES=['./','./index.html','./logo.png','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
